3797c2003d41d308e7dc53eff39204719a851329
Yuriy Naydenov <ebceu44@gmail.com>
2018-09-18 14:23:03 +0300

    docs

--- build/build.ts
+++ build/build.ts
@@ -24,30 +24,35 @@ const files = (path: string, filter: (file: string) => boolean = (_) => true): P
   new Promise((resolve, reject) => readdir(path, (err, files) => err ? reject(err) : resolve(files.filter(filter))))
 
 async function build() {
-  await remove(p('tmp'))
-  await create(p('tmp'))
-  await create(p('tmp/src'))
-  //  await run('npm pack waves-transactions', p('tmp'))
-  //  const tgz = (await files(p('tmp'), f => f.startsWith('waves-transactions-')))[0]
-  //  await run(`tar zxvf ${tgz}`, p('tmp'))
-  //  await create(p('tmp/node_modules'))
-  //  await copy(p('tmp/package'), p('tmp/node_modules/waves-transactions'))
+  try {
+    await remove(p('tmp'))
+    await create(p('tmp'))
+    await create(p('tmp/src'))
+    //  await run('npm pack waves-transactions', p('tmp'))
+    //  const tgz = (await files(p('tmp'), f => f.startsWith('waves-transactions-')))[0]
+    //  await run(`tar zxvf ${tgz}`, p('tmp'))
+    //  await create(p('tmp/node_modules'))
+    //  await copy(p('tmp/package'), p('tmp/node_modules/waves-transactions'))
 
-
-  await copy(p('../src'), p('tmp/src'))
-  await copy(p('../usage'), p('tmp/usage'))
-  await copy(p('../tsconfig.json'), p('tmp/tsconfig.json'))
-  await run('tsc', p('tmp'))
-  await create(p('tmp/node_modules'))
-  await copy(p('tmp/dist'), p('tmp/node_modules/waves-transactions'))
-  await copy(p('package.json'), p('tmp/node_modules/waves-transactions/package.json'))
-  await remove(p('tmp/dist'))
-  await run('ts-node usage/index.ts', p('tmp'))
-  await run('tsc', p('tmp'))
-  await copy(p('package.json'), p('tmp/dist/package.json'))
-  await copy(p('../README.md'), p('tmp/dist/README.md'))
-  await copy(p('tmp/dist'), p('../dist'))
-  await remove(p('tmp'))
+    await copy(p('../src'), p('tmp/src'))
+    await copy(p('../usage'), p('tmp/usage'))
+    await copy(p('../tsconfig.json'), p('tmp/tsconfig.json'))
+    await run('tsc', p('tmp'))
+    await create(p('tmp/node_modules'))
+    await copy(p('tmp/dist'), p('tmp/node_modules/waves-transactions'))
+    await copy(p('package.json'), p('tmp/node_modules/waves-transactions/package.json'))
+    await remove(p('tmp/dist'))
+    await run('ts-node usage/index.ts', p('tmp'))
+    await run('typedoc', p('tmp'))
+    await run('tsc', p('tmp'))
+    await copy(p('package.json'), p('tmp/dist/package.json'))
+    await copy(p('../README.md'), p('tmp/dist/README.md'))
+    await copy(p('tmp/dist'), p('../dist'))
+    await copy(p('tmp/docs'), p('../docs'))
+    await remove(p('tmp'))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 build()
