a3f04371a2c6a55e9d63aefeeb0a5383dd18d0d7
Yuriy Naydenov <ebceu44@gmail.com>
2018-10-08 16:30:08 +0300

    transfer doc fix

--- usage/transfer.ts
+++ usage/transfer.ts
@@ -2,9 +2,9 @@ import { transfer } from '../dist/index'
 
 const seed = 'b716885e9ba64442b4f1263c8e2d8671e98b800c60ec4dc2a27c83e5f9002b18'
 
-//Transfering 100 WAVES
+//Transfering 1 WAVES
 const params = {
-  amount: 100,
+  amount: 100000000,
   recipient: '3P23fi1qfVw6RVDn4CH2a5nNouEtWNQ4THs',
   //feeAssetId: undefined
   //assetId: undefined
