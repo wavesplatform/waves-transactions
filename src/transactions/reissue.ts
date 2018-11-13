f84735111e6e43b4198adfaf36ef53e71a481eca
siem <siemarell@gmail.com>
2018-11-13 12:03:32 +0300

    reissue

--- src/transactions/reissue.ts
+++ src/transactions/reissue.ts
@@ -1,7 +1,10 @@
 import { TransactionType, ReissueTransaction } from "../transactions"
 import { concat, BASE58_STRING, LONG, signBytes, hashBytes, BYTES, BOOL } from "waves-crypto"
-import { pullSeedAndIndex, addProof, valOrDef, mapSeed, getSenderPublicKey } from "../generic"
-import { SeedTypes, Params} from "../types";
+import { pullSeedAndIndex, addProof, mapSeed, getSenderPublicKey } from "../generic"
+import { SeedTypes, Params } from "../types";
+import { noError, ValidationResult } from "waves-crypto/validation";
+import { generalValidation, raiseValidationErrors } from "../validation";
+import { VALIDATOR_MAP } from "../schemas";
 
 export interface ReissueParams extends Params {
   assetId: string
@@ -12,38 +15,43 @@ export interface ReissueParams extends Params {
   chainId?: string
 }
 
+export const reissueValidation = (tx: ReissueTransaction): ValidationResult => [
+  noError
+]
+
+export const reissueToBytes = (tx: ReissueTransaction): Uint8Array => concat(
+  BYTES([TransactionType.Reissue, tx.version, tx.chainId.charCodeAt(0)]),
+  BASE58_STRING(tx.senderPublicKey),
+  BASE58_STRING(tx.assetId),
+  LONG(tx.quantity),
+  BOOL(tx.reissuable),
+  LONG(tx.fee),
+  LONG(tx.timestamp)
+)
+
 /* @echo DOCS */
 export function reissue(paramsOrTx: ReissueParams | ReissueTransaction, seed?: SeedTypes): ReissueTransaction {
   const { nextSeed } = pullSeedAndIndex(seed)
-  const { assetId, quantity, chainId, reissuable, fee, timestamp } = paramsOrTx
 
   const senderPublicKey = getSenderPublicKey(seed, paramsOrTx)
 
-  const proofs = (<any>paramsOrTx)['proofs']
-  const tx: ReissueTransaction = proofs && proofs.length > 0 ?
-    paramsOrTx as ReissueTransaction : {
-      type: TransactionType.Reissue,
-      version: 2,
-      assetId,
-      quantity,
-      chainId: chainId || 'W',
-      reissuable,
-      fee: valOrDef(fee, 100000000),
-      senderPublicKey,
-      timestamp: valOrDef(timestamp, Date.now()),
-      proofs: [],
-      id: ''
-    }
+  const tx: ReissueTransaction = {
+    type: TransactionType.Reissue,
+    version: 2,
+    chainId: 'W',
+    fee: 100000000,
+    senderPublicKey,
+    timestamp: Date.now(),
+    proofs: [],
+    id: '',
+    ...paramsOrTx
+  }
 
-  const bytes = concat(
-    BYTES([TransactionType.Reissue, tx.version, tx.chainId.charCodeAt(0)]),
-    BASE58_STRING(tx.senderPublicKey),
-    BASE58_STRING(tx.assetId),
-    LONG(tx.quantity),
-    BOOL(tx.reissuable),
-    LONG(tx.fee),
-    LONG(tx.timestamp)
-  )
+  raiseValidationErrors(
+    generalValidation(tx, VALIDATOR_MAP['ReissueTransaction']),
+    reissueValidation(tx)
+)
+  const bytes = reissueToBytes(tx)
 
   mapSeed(seed, (s, i) => addProof(tx, signBytes(bytes, s), i))
   tx.id = hashBytes(bytes)
