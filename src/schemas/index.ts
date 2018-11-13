396a27cb2fcf80a6266a87e133a4d84732adc7f7
siem <siemarell@gmail.com>
2018-11-13 18:06:15 +0300

    fixed type

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,8 +5,10 @@ const ajv = Ajv({
   allErrors: true
 })
 
-const mapObj = <T>(obj: any, f:(v:any)=>any): {[key:string]: ValidateFunction} => {
-  return Object.entries(obj).map(([k,v]) => [k, f(v)]).reduce((acc, [k,v]) => ({...acc,[k]:v}), {} as any)
-}
 
-export const VALIDATOR_MAP = mapObj(schemas, ajv.compile.bind(ajv))
+const mapObj = <T, U, K extends string>(obj: Record<K, T>, f:(v: T)=> U): Record<K, U> =>
+  Object.entries<T>(obj).map(([k,v]) => [k, f(v)] as [string, U])
+    .reduce((acc, [k,v]) => ({...acc as any, [k]: v}), {} as Record<K,U>)
+
+
+export const VALIDATOR_MAP = mapObj(schemas, (schema) => ajv.compile(schema));
