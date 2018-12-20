4eaf9db384a3ea0125c6fdf7b2f7a9dadce0c756
siem <siemarell@gmail.com>
2018-12-20 17:27:37 +0300

    v3.0.4

--- test/transactions/contract-invocation.test.ts
+++ test/transactions/contract-invocation.test.ts
@@ -1,6 +1,7 @@
 import { publicKey, verifySignature } from 'waves-crypto'
 import { contractInvocationMinimalParams } from '../minimalParams'
 import { contractInvocation } from "../../src/transactions/contract-invocation";
+import { binary } from "@waves/marshall";
 
 describe('contractInvocation', () => {
 
@@ -11,27 +12,32 @@ describe('contractInvocation', () => {
     expect(tx).toMatchObject({ ...contractInvocationMinimalParams })
   });
 
+  it('should build from minimal set of params with payment', () => {
+    const tx = contractInvocation({ ...contractInvocationMinimalParams, payment:{amount:100} }, stringSeed)
+    expect(tx).toMatchObject({ ...contractInvocationMinimalParams })
+  });
+
   it('should build from minimal set of params with additional properties', () => {
     const tx = contractInvocation({ ...contractInvocationMinimalParams, a: 10000 } as any, stringSeed)
     expect(tx).toMatchObject({ ...contractInvocationMinimalParams })
   });
 
 
-  // it('Should get correct signature', () => {
-  //   const tx = contractInvocation({ ...contractInvocationMinimalParams }, stringSeed)
-  //   expect(verifySignature(publicKey(stringSeed), aliasToBytes(tx), tx.proofs[0]!)).toBeTruthy()
-  // });
-  //
-  // it('Should sign already signed', () => {
-  //   let tx = contractInvocation({ ...contractInvocationMinimalParams }, stringSeed);
-  //   tx = contractInvocation(tx, stringSeed);
-  //   expect(verifySignature(publicKey(stringSeed), aliasToBytes(tx), tx.proofs[1]!)).toBeTruthy()
-  // });
-  //
-  // it('Should get correct multiSignature', () => {
-  //   const stringSeed2 = 'example seed 2'
-  //   const tx = contractInvocation({ ...contractInvocationMinimalParams }, [null, stringSeed, null, stringSeed2])
-  //   expect(verifySignature(publicKey(stringSeed), aliasToBytes(tx), tx.proofs[1]!)).toBeTruthy()
-  //   expect(verifySignature(publicKey(stringSeed2), aliasToBytes(tx), tx.proofs[3]!)).toBeTruthy()
-  // })
+  it('Should get correct signature', () => {
+    const tx = contractInvocation({ ...contractInvocationMinimalParams }, stringSeed)
+    expect(verifySignature(publicKey(stringSeed), binary.serializeTx(tx), tx.proofs[0]!)).toBeTruthy()
+  });
+
+  it('Should sign already signed', () => {
+    let tx = contractInvocation({ ...contractInvocationMinimalParams }, stringSeed);
+    tx = contractInvocation(tx, stringSeed);
+    expect(verifySignature(publicKey(stringSeed),  binary.serializeTx(tx), tx.proofs[1]!)).toBeTruthy()
+  });
+
+  it('Should get correct multiSignature', () => {
+    const stringSeed2 = 'example seed 2'
+    const tx = contractInvocation({ ...contractInvocationMinimalParams, payment:{amount:100} }, [null, stringSeed, null, stringSeed2])
+    expect(verifySignature(publicKey(stringSeed),  binary.serializeTx(tx), tx.proofs[1]!)).toBeTruthy()
+    expect(verifySignature(publicKey(stringSeed2),  binary.serializeTx(tx), tx.proofs[3]!)).toBeTruthy()
+  })
 })
