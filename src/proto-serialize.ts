bee22bac2a4a78a1e4c58aeb6075733f7ab17320
siem <siemarell@gmail.com>
2019-11-07 16:59:26 +0300

    :construction: deserialization

--- src/proto-serialize.ts
+++ src/proto-serialize.ts
@@ -1,5 +1,12 @@
 import * as wavesProto from '@waves/protobuf-serialization'
-import { base58Decode, base58Encode, stringToBytes, base64Decode } from '@waves/ts-lib-crypto'
+import {
+  base58Decode,
+  base58Encode,
+  stringToBytes,
+  base64Decode,
+  bytesToString,
+  base64Encode
+} from '@waves/ts-lib-crypto'
 import { binary, schemas } from '@waves/marshall'
 import {
   IAliasTransaction,
@@ -17,32 +24,124 @@ import {
   ISetScriptTransaction, ISponsorshipTransaction,
   ITransaction,
   ITransferTransaction,
-  TOrder,
+  TOrder, TRANSACTION_TYPE, TTransactionType,
   TTx
 } from './transactions'
-import { isOrder } from './generic'
+import { base64Prefix, fee, isOrder } from './generic'
 import Long from 'long'
+import { lease } from './transactions/lease'
 
-export function serializeToProtoBytes(obj: TTx | TOrder): Uint8Array {
-  if (isOrder(obj)) return null as any
-  const converted = convert(obj)
-  return wavesProto.waves.Transaction.encode(converted as any).finish()
+export function txToProtoBytes(obj: TTx): Uint8Array {
+  return wavesProto.waves.Transaction.encode(txToProto(obj)).finish()
 }
 
-export function parseProtoBytes(bytes: Uint8Array) {
-  const { senderPublicKey, fee, timestamp, data, version, ...rest } = wavesProto.waves.Transaction.decode(bytes)
+export function orderToProtoBytes(obj: TOrder): Uint8Array {
+  return wavesProto.waves.Order.encode(orderToProto(obj)).finish()
+}
 
-  return {
-    version,
-    type: typeByName[data!],
-    senderPublicKey: base58Encode(senderPublicKey),
-    timestamp: timestamp.toString(),
-    fee,
-    ...rest[data!]
+export function protoBytesToTx(bytes: Uint8Array): TTx {
+  const t = wavesProto.waves.Transaction.decode(bytes)
+
+  let res: any = {
+    version: t.version,
+    type: typeByName[t.data!] as TTransactionType,
+    senderPublicKey: base58Encode(t.senderPublicKey),
+    timestamp: t.timestamp.toString(),
+    fee: t.fee!.amount!.toString(),
   }
+  if (t.fee!.assetId != null) {
+    res.feeAssetId = base58Encode(t.fee!.assetId)
+  }
+  switch (t.data) {
+    case 'issue':
+      res.name = bytesToString(t.issue!.name!)
+      res.description = bytesToString(t.issue!.description!)
+      res.quantity = t.issue!.amount!.toString()
+      res.decimals = t.issue!.decimals
+      res.reissuable = t.issue!.reissuable
+      res.script = t.issue!.script && base64Prefix(base64Encode(t.issue!.script.bytes!))
+      break
+    case 'transfer':
+      res.amount = t.transfer!.amount!.amount!.toString()
+      res.recipient = t.transfer!.recipient!.alias ? t.transfer!.recipient!.alias : base58Encode(t.transfer!.recipient!.address!)
+      res.attachment = t.transfer!.attachment == null ? null : base58Encode(t.transfer!.attachment)
+      res.assetId = t.transfer!.amount!.assetId == null ? null : base58Encode(t.transfer!.amount!.assetId)
+      break
+    case "reissue":
+      res.quantity = t.reissue!.assetAmount!.amount!.toString()
+      res.assetId = t.reissue!.assetAmount!.assetId == null ? null : base58Encode(t.reissue!.assetAmount!.assetId)
+      res.reissuable = t.reissue!.reissuable
+      break
+    case "burn":
+      res.quantity = t.burn!.assetAmount!.amount!.toString()
+      res.assetId = base58Encode(t.burn!.assetAmount!.assetId!)
+      res.amount = t.exchange
+      res.price = t.exchange
+      res.buyMatcherFee = t.exchange
+      res.sellMatcherFee = t.exchange
+      res.order1 = t.exchange
+      res.order2 = t.exchange
+      break
+    case "exchange":
+      res.amount = t.exchange!.amount!.toString()
+      res.price = t.exchange!.price!.toString()
+      res.buyMatcherFee = t.exchange!.buyMatcherFee!.toString()
+      res.sellMatcherFee = t.exchange!.sellMatcherFee!.toString()
+      res.order1 = orderFromProto(t.exchange!.orders![0])
+      res.order2 = orderFromProto(t.exchange!.orders![1])
+      break
+    case "lease":
+      res.recipient = t.lease!.recipient!.alias ? t.lease!.recipient!.alias : base58Encode(t.lease!.recipient!.address!)
+      res.amount = t.lease!.amount!.toString()
+      break
+    case "leaseCancel":
+      res.leaseId = base58Encode(t.leaseCancel!.leaseId!)
+      break
+    case "createAlias":
+      res.alias = t.createAlias!.alias
+      break
+    case "massTransfer":
+      res.assetId = t.massTransfer!.assetId == null ? null : base58Encode(t.massTransfer!.assetId)
+      res.attachment = t.massTransfer!.attachment == null ? null : base58Encode(t.massTransfer!.attachment)
+      res.transfers = t.massTransfer!.transfers!.map(({ amount, address }) => ({
+        amount: amount!.toString(),
+        address: address!.alias ? address!.alias : base58Encode(address!.address!)
+      }))
+      break
+    case "dataTransaction":
+      res.data = t.dataTransaction!.data!.map(de => {
+        if (de!.binaryValue) return { key: de.key, type: 'binary', value: base64Encode(de.binaryValue!) }
+        if (de!.boolValue) return { key: de.key, type: 'boolean', value: de.boolValue }
+        if (de!.intValue) return { key: de.key, type: 'integer', value: de.intValue!.toString() }
+        if (de!.stringValue) return { key: de.key, type: 'string', value: de.stringValue }
+      })
+      break
+    case "setScript":
+      res.script = t.setScript!.script == null ? null : base64Prefix(base64Encode(t.setScript!.script!.bytes!))
+      break
+    case "sponsorFee":
+      res.minSponsoredAssetFee = t.sponsorFee!.minFee!.amount!.toString()
+      res.assetId = base58Encode(t.sponsorFee!.minFee!.assetId!)
+      break
+    case "setAssetScript":
+      res.assetId = base58Encode(t.setAssetScript!.assetId!)
+      res.script = base64Prefix(base64Encode(t.setAssetScript!.script!.bytes!))
+      break
+    case "invokeScript":
+      res.dApp = t.invokeScript!.dApp!.alias ? t.invokeScript!.dApp!.alias : base58Encode(t.invokeScript!.dApp!.address!)
+      res.functionCall = ''
+      res.payment = t.invokeScript!.payments!.map(p => ({amount: p.amount!.toString(), assetId: p.assetId == null ? null : base58Encode(p.assetId)}))
+      break
+    default:
+      throw new Error(`Unsopported tx type ${t.data}`)
+  }
+  if (t.fee!.assetId) {
+    (res as any).feeAssetId = t.fee!.assetId
+  }
+  return res
 }
 
-const convertCommon = ({ senderPublicKey, fee, timestamp, type, version, chainId, ...rest }: TTx) => {
+const getCommonFields = ({ senderPublicKey, fee, timestamp, type, version, chainId, ...rest }: TTx) => {
   const typename = nameByType[type]
   return {
     version,
@@ -54,7 +153,6 @@ const convertCommon = ({ senderPublicKey, fee, timestamp, type, version, chainId
     data: typename,
   }
 }
-
 const getIssueData = (t: IIssueTransaction): wavesProto.waves.IIssueTransactionData => ({
   name: stringToBytes(t.name),
   description: stringToBytes(t.description),
@@ -114,6 +212,55 @@ const getInvokeData = (t: IInvokeScriptTransaction): wavesProto.waves.IInvokeScr
   payments: t.payment == null ? null : t.payment.map(({ amount, assetId }) => amountToProto(amount, assetId))
 })
 
+const txToProto = (t: TTx): wavesProto.waves.ITransaction => {
+  const common = getCommonFields(t)
+  let txData
+  switch (t.type) {
+    case TRANSACTION_TYPE.ISSUE:
+      txData = getIssueData(t)
+      break
+    case TRANSACTION_TYPE.TRANSFER:
+      txData = getTransferData(t)
+      break
+    case TRANSACTION_TYPE.REISSUE:
+      txData = getReissueData(t)
+      break
+    case TRANSACTION_TYPE.BURN:
+      txData = getBurnData(t)
+      break
+    case TRANSACTION_TYPE.LEASE:
+      txData = getLeaseData(t)
+      break
+    case TRANSACTION_TYPE.CANCEL_LEASE:
+      txData = getCancelLeaseData(t)
+      break
+    case TRANSACTION_TYPE.ALIAS:
+      txData = getAliasData(t)
+      break
+    case TRANSACTION_TYPE.MASS_TRANSFER:
+      txData = getMassTransferData(t)
+      break
+    case TRANSACTION_TYPE.DATA:
+      txData = getDataTxData(t)
+      break
+    case TRANSACTION_TYPE.SET_SCRIPT:
+      txData = getSetScriptData(t)
+      break
+    case TRANSACTION_TYPE.SET_ASSET_SCRIPT:
+      txData = getSetAssetScriptData(t)
+      break
+    case TRANSACTION_TYPE.SPONSORSHIP:
+      txData = getSponsorData(t)
+      break
+    case TRANSACTION_TYPE.EXCHANGE:
+      txData = getExchangeData(t)
+      break
+    case TRANSACTION_TYPE.INVOKE_SCRIPT:
+      txData = getInvokeData(t)
+      break
+  }
+  return { ...common, [common.data]: txData }
+}
 const orderToProto = (o: IOrder): wavesProto.waves.IOrder => ({
   chainId: (o as any).chainId,
   senderPublicKey: base58Decode(o.senderPublicKey),
@@ -122,7 +269,7 @@ const orderToProto = (o: IOrder): wavesProto.waves.IOrder => ({
     amountAssetId: o.assetPair.amountAsset == null ? null : base58Decode(o.assetPair.amountAsset),
     priceAssetId: o.assetPair.priceAsset == null ? null : base58Decode(o.assetPair.priceAsset)
   },
-  orderSide: o.orderType === "buy" ? wavesProto.waves.Order.Side.BUY :  wavesProto.waves.Order.Side.SELL,
+  orderSide: o.orderType === "buy" ? wavesProto.waves.Order.Side.BUY : wavesProto.waves.Order.Side.SELL,
   amount: Long.fromValue(o.amount),
   price: Long.fromValue(o.price),
   timestamp: Long.fromValue(o.timestamp),
@@ -131,6 +278,26 @@ const orderToProto = (o: IOrder): wavesProto.waves.IOrder => ({
   version: o.version,
   proofs: o.proofs.map(base58Decode),
 })
+
+const orderFromProto = (po: wavesProto.waves.IOrder): TOrder => ({
+  version: po.version! as 1 | 2 | 3,
+  senderPublicKey: base58Encode(po.senderPublicKey!),
+  matcherPublicKey: base58Encode(po.matcherPublicKey!),
+  assetPair: {
+    amountAsset: po!.assetPair!.amountAssetId == null ? null : base58Encode(po!.assetPair!.amountAssetId),
+    priceAsset: po!.assetPair!.priceAssetId == null ? null : base58Encode(po!.assetPair!.priceAssetId)
+  },
+  // @ts-ignore
+  chainId: po.chainId,
+  orderType: po.orderSide === wavesProto.waves.Order.Side.BUY ? 'buy' : 'sell',
+  amount: po.amount!.toString(),
+  price: po.price!.toString(),
+  timestamp: po.timestamp!.toNumber(),
+  expiration: po.expiration!.toNumber(),
+  matcherFee: po.matcherFee!.amount!.toNumber(),
+  matcherFeeAssetId: po.matcherFee!.assetId == null ? null : base58Encode(po.matcherFee!.assetId),
+})
+
 const recipientToProto = (r: string): wavesProto.waves.IRecipient => ({
   alias: r.startsWith('alias') ? r : undefined,
   address: !r.startsWith('alias') ? base58Decode(r) : undefined
@@ -191,20 +358,3 @@ const typeByName = {
   "invokeScript": 16 as 16,
 }
 
-const fieldsToConvert = ['fee', 'timestamp', 'amount', 'quantity']
-const convertToLong = (tx: TTx): ITransaction<Long> => {
-  const go = (obj: any, convert: boolean = false): ITransaction<Long> => {
-    if (Array.isArray(obj)) {
-      return obj.map(item => go(item)) as any
-    } else if (typeof obj === 'object') {
-      return Object.entries(obj).reduce((acc, [k, v]) => {
-        const c = fieldsToConvert.includes(k)
-        acc[k] = go(v, c)
-        return acc
-      }, {} as any)
-    } else {
-      return convert ? Long.fromValue(obj) : obj
-    }
-  }
-  return go(tx)
-}
