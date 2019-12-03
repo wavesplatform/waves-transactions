767c3c070d2d48a691f430f46cda092a2d8c904e
siem <siemarell@gmail.com>
2019-12-03 17:32:56 +0300

    order left

--- src/proto-serialize.ts
+++ src/proto-serialize.ts
@@ -256,7 +256,7 @@ const getExchangeData = (t: IExchangeTransaction): wavesProto.waves.IExchangeTra
   price: Long.fromValue(t.price),
   buyMatcherFee: Long.fromValue(t.buyMatcherFee),
   sellMatcherFee: Long.fromValue(t.sellMatcherFee),
-  orders: [orderToProto(t.order1), orderToProto(t.order2)],
+  orders: [orderToProto({chainId: t.chainId, ...t.order1}), orderToProto({chainId: t.chainId, ...t.order2})],
 })
 const getLeaseData = (t: ILeaseTransaction): wavesProto.waves.ILeaseTransactionData => ({
   recipient: recipientToProto(t.recipient),
@@ -343,8 +343,8 @@ export const txToProto = (t: TTx): wavesProto.waves.ITransaction => {
   }
   return { ...common, [common.data]: txData }
 }
-const orderToProto = (o: IOrder): wavesProto.waves.IOrder => ({
-  chainId: (o as any).chainId,
+const orderToProto = (o: IOrder & {chainId: number}): wavesProto.waves.IOrder => ({
+  chainId: o.chainId,
   senderPublicKey: base58Decode(o.senderPublicKey),
   matcherPublicKey: base58Decode(o.matcherPublicKey),
   assetPair: {
