6aa3a8364cf28dc37a7e9281434605e00172fcd4
Maxim Smolyakov <1939299+msmolyakov@users.noreply.github.com>
2020-03-26 21:16:58 +0300

    fix testnet url in config.ts

--- test/integration/config.ts
+++ test/integration/config.ts
@@ -4,11 +4,11 @@ import { randomBytes } from '@waves/ts-lib-crypto'
  * Before running test ensure MASTER_SEED has at leas 10 WAVES!!
  */
 export const MASTER_SEED = 'test acc 2'
-export const API_BASE = 'https://testnodes.wavesnodes.com'
+export const API_BASE = 'https://nodes-testnet.wavesnodes.com'
 export const CHAIN_ID = 'T'
 
 export const MATCHER_PUBLIC_KEY = '8QUAqtTckM5B8gvcuP7mMswat9SjKUuafJMusEoSn1Gy'
-export const MATCHER_URL = 'https://matcher.testnet.wavesnodes.com/'
+export const MATCHER_URL = 'https://matcher-testnet.wavesnodes.com/'
 
 export const TIMEOUT = 200000
 
