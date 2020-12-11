61332e33e96d7ab74dd393b5903587ca4413ff7d
Daniil <lobanovdanil97@gmail.com>
2020-12-11 12:45:11 +0300

    fix attachments types

--- src/transactions/transfer.ts
+++ src/transactions/transfer.ts
@@ -34,7 +34,7 @@ export function transfer(paramsOrTx: any, seed?: TSeedTypes): ITransferTransacti
     assetId: normalizeAssetId(paramsOrTx.assetId),
     recipient: paramsOrTx.recipient,
     amount: paramsOrTx.amount,
-    attachment: paramsOrTx.attachment,
+    attachment: paramsOrTx.attachment || '',
     fee: fee(paramsOrTx, 100000),
     feeAssetId: normalizeAssetId(paramsOrTx.feeAssetId),
     timestamp: paramsOrTx.timestamp || Date.now(),
