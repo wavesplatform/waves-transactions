3087d98f412bd7a84f087f7396c3aa8e044d8bc9
Daniil <lobanovdanil97@gmail.com>
2021-04-13 17:30:11 +0300

    wip/invoke-2

--- test/transactions/invoke.test.ts
+++ test/transactions/invoke.test.ts
@@ -1,64 +1,44 @@
-import {protoBytesToTx, txToProtoBytes} from '../../src/proto-serialize'
-import {signTx} from '../../src'
-import {signBytes} from '@waves/ts-lib-crypto'
+import {broadcast, invokeScript, waitForTx} from '../../src'
+import {TIMEOUT} from '../integration/config'
+import {TRANSACTION_TYPE} from '../../src/transactions'
 
 const dataTestnet = {
-    type: 16,
+    type: TRANSACTION_TYPE.INVOKE_SCRIPT,
     version: 2,
-    dApp: '3NBfVHNKz2UWj4pH2XE9fG27GGZCWkt7XBt',
-    call: {'function': "call",
-        "args": [{"type": "list", "value": [{"type": "integer", "value": 1}]}, {
-            "type": "list",
-            "value": [{"type": "string", "value": "123"}]
-        }]
+    dApp: '3N5DJuwBeYpyATrWqFc5DqcKLPGHfNEtFyX',
+    call: {function: 'sometest',
+        args: [{type: 'integer', value: '1'},
+            {type: 'string', value: 'qwerty'}],
     },
-    senderPublicKey: "FCfxuzbxcMUuTgtn6bHJcwhTEjhSdh1ppnQn9wnfvnyL",
     chainId: 84,
+    senderPublicKey: '5uETz7mMqdRRkSoPTJZ1p5qWR5jwhCxQwUKqSzgBaTaX',
     fee: 1000000,
-    timestamp: 1618311766111,
-    id: "MpwqRQGv7bq7Ndfg1sbLFhGmzWEtiEQXqAUnZiYDgDY",
-    script: "base64:AAIEAAAAAAAAAAgIAhIECgIRGAAAAAAAAAABAAAAAWkBAAAABGNhbGwAAAACAAAABWxpc3RBAAAABWxpc3RCCQAETAAAAAIJAQAAAAxJbnRlZ2VyRW50cnkAAAACAgAAAANpbnQJAAGRAAAAAgUAAAAFbGlzdEEAAAAAAAAAAAAJAARMAAAAAgkBAAAAC1N0cmluZ0VudHJ5AAAAAgIAAAADaW50CQABkQAAAAIFAAAABWxpc3RCAAAAAAAAAAAABQAAAANuaWwAAAAAl/I1IA=="
+    proofs: [],
 }
 
 const dataMainnet = {
-    type: 16,
+    type: TRANSACTION_TYPE.INVOKE_SCRIPT,
     version: 2,
-    dApp: '3NBfVHNKz2UWj4pH2XE9fG27GGZCWkt7XBt',
-    call: {'function': "call",
-        "args": [{"type": "list", "value": [{"type": "integer", "value": 1}]}, {
-            "type": "list",
-            "value": [{"type": "string", "value": "123"}]
-        }]
+    dApp: '3N5DJuwBeYpyATrWqFc5DqcKLPGHfNEtFyX',
+    call: {function: 'sometest',
+        args: [{type: 'integer', value: '1'},
+            {type: 'string', value: 'qwerty'}],
     },
-    senderPublicKey: "FCfxuzbxcMUuTgtn6bHJcwhTEjhSdh1ppnQn9wnfvnyL",
     chainId: 87,
+    senderPublicKey: '5uETz7mMqdRRkSoPTJZ1p5qWR5jwhCxQwUKqSzgBaTaX',
     fee: 1000000,
-    timestamp: 1618311766111,
-    id: "MpwqRQGv7bq7Ndfg1sbLFhGmzWEtiEQXqAUnZiYDgDY",
-    script: "base64:AAIEAAAAAAAAAAgIAhIECgIRGAAAAAAAAAABAAAAAWkBAAAABGNhbGwAAAACAAAABWxpc3RBAAAABWxpc3RCCQAETAAAAAIJAQAAAAxJbnRlZ2VyRW50cnkAAAACAgAAAANpbnQJAAGRAAAAAgUAAAAFbGlzdEEAAAAAAAAAAAAJAARMAAAAAgkBAAAAC1N0cmluZ0VudHJ5AAAAAgIAAAADaW50CQABkQAAAAIFAAAABWxpc3RCAAAAAAAAAAAABQAAAANuaWwAAAAAl/I1IA=="
+    proofs: [],
 }
 
-const seed = 'cover couch auto wife acoustic venue economy scan april noise paper horn scene refuse need'
+const seed = 'release melody settle mad undo combine rich rug artist reason crucial genius fiction bid click'
 
 describe('data', () => {
 
+    it('Broadcast script-invocation', async () => {
+        // @ts-ignore
+        const invokeTx = invokeScript(dataTestnet, seed)
+        await broadcast(invokeTx, 'https://nodes-testnet.wavesnodes.com')
 
-    test.each([
-        [
-            dataTestnet,
-            'dataTestnet',
-        ],
-        [
-            dataMainnet,
-            'dataMainnet',
-        ],
-    ])('check serialization for %o, tx version: %i', (data, name) => {
-        const tx = data
-        const fs = require('fs')
-        fs.writeFileSync(`../${name}_bytes.txt`, txToProtoBytes(tx))
-        console.log(txToProtoBytes(tx))
-        const bytes = txToProtoBytes(tx)
-        const signedBytes = signBytes(seed, bytes)
-        fs.writeFileSync(`../${name}_signedBytes.txt`, signedBytes)
-    })
+        await waitForTx(invokeTx.id, { timeout: TIMEOUT, apiBase: 'https://nodes-testnet.wavesnodes.com' })
+    }, TIMEOUT)
 })
