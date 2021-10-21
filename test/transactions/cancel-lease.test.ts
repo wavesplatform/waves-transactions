0da3d30c39fe5418d114bb84002ddeb3ae064452
Alex <rutol@mail.ru>
2021-10-21 20:36:43 +0300

    Update cancel-lease.test.ts

--- test/transactions/cancel-lease.test.ts
+++ test/transactions/cancel-lease.test.ts
@@ -1,5 +1,5 @@
 import { publicKey } from '@waves/ts-lib-crypto'
-import { cancelLease } from '../../src'
+import {cancelLease, ICancelLeaseParams} from '../../src'
 import { cancelLeaseMinimalParams } from '../minimalParams'
 import { validateTxSignature } from '../../test/utils'
 
@@ -30,5 +30,38 @@ describe('cancel-lease', () => {
 
     expect(validateTxSignature(tx, 2, 1, publicKey(stringSeed))).toBeTruthy()
     expect(validateTxSignature(tx, 2, 3, publicKey(stringSeed2))).toBeTruthy()
+  })
+
+  const cancelLeaseTestParams = {
+    leaseId: "DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr",
+    originTransactionId: "BhHPPHBZpfp8FBy8DE7heTpWGJySYg2uU2r4YM6qaisw",
+    sender: "3Mx7kNAFcGrAeCebnt3yXceiRSwru6N3XZd",
+    recipient: "3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7",
+    amount: 124935000,
+    height: 1551763,
+    status: "canceled"
+  }
+
+  //const leaseId = "DT5bC1S6XfpH7s4hcQQkLj897xnnXQPNgYbohX7zZKcr"
+
+  it('Should build from minimal set of params check fee', () => {
+    const tx = cancelLease({ ...cancelLeaseMinimalParams }, stringSeed)
+    expect(tx.fee).toEqual(100000)
+  })
+
+  it('Should build from minimal params set fee', () => {
+    const tx = cancelLease({ ...cancelLeaseMinimalParams, fee: 500000 }, stringSeed)
+    expect(tx.fee).toEqual(500000)
+  })
+
+  it('Should build with complex params', () => {
+    const tx = cancelLease({ ...cancelLeaseTestParams }, stringSeed)
+    //expect(tx.fee).toEqual(-1)
+    expect(tx).toMatchObject({ ...cancelLeaseTestParams })
+  })
+
+  it('Should build with negative fee', () => {
+    const tx = cancelLease({ ...cancelLeaseMinimalParams, fee: -1 }, stringSeed)
+    expect(tx.fee).toEqual(-1)
   })
 })
