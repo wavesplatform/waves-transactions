e352cc5a8764ef9521982ef757f8dee9e8d96d46
Alex <rutol@mail.ru>
2021-10-22 23:39:45 +0300

    Create sponsorship.test.ts

---
+++ test/transactions/sponsorship.test.ts
@@ -0,0 +1,12 @@
+import { publicKey, } from '@waves/ts-lib-crypto'
+import { setScript } from '../../src'
+import { validateTxSignature } from '../../test/utils'
+
+describe('setSponsorship', () => {
+
+    it( 'Should create sponsorship transaction', () => {
+        
+    })
+
+
+})
