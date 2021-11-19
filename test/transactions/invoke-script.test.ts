562a4288efbdff1700aa3cf22d4bd39a67c9d73e
Alex <rutol@mail.ru>
2021-11-19 13:24:19 +0300

    Update invoke-script.test.ts

--- test/transactions/invoke-script.test.ts
+++ test/transactions/invoke-script.test.ts
@@ -3,6 +3,7 @@ import { invokeScriptMinimalParams } from '../minimalParams'
 import { invokeScript } from '../../src/transactions/invoke-script'
 import { IInvokeScriptParams } from '../../src'
 import { validateTxSignature } from '../../test/utils'
+import {binary} from "@waves/marshall";
 
 describe('invokeScript', () => {
 
@@ -69,6 +70,52 @@ describe('invokeScript', () => {
     expect(validateTxSignature(tx, 1, 3, publicKey(stringSeed2))).toBeTruthy()
   })
 
+  const testInvokeScriptParams= {
+    dApp: '3N3Cn2pYtqzj7N9pviSesNe8KG9Cmb718Y1',
+    call: {
+      function: 'foo',
+      args: [
+        {
+          type: 'binary',
+          value: 'base64:AQa3b8tH',
+        },
+        {
+          type: 'boolean',
+          value: true,
+        },
+        {
+          type: 'integer',
+          value: 1234567890,
+        },
+        {
+          type: 'string',
+          value: "Abra Shvabra Kadabra",
+        },
+        {
+          type: 'list',
+          value: [
+            {
+              type: 'binary',
+              value: 'base64:UmlkZQ=='
+            },
+            {
+              type: 'boolean',
+              value: false
+            },
+            {
+              type: 'integer',
+              value: 223322
+            },
+            {
+              type: 'string',
+              value: "Porto Franko"
+            }
+          ]
+        }
+      ]
+    },
+  }
+
   it('Should build with minimal fee', () => {
     const tx = invokeScript({ ...invokeScriptMinimalParams, fee: 100000 }, stringSeed)
     expect(tx.fee).toEqual(100000)
@@ -84,4 +131,20 @@ describe('invokeScript', () => {
     expect(tx.fee).toEqual(-1)
   })
 
+  it('Should build with test set params', () => {
+    const tx = invokeScript(testInvokeScriptParams, stringSeed)
+    expect(tx.call!.args[0].value).toEqual('base64:AQa3b8tH')
+    expect(tx.call!.args[1].value).toEqual(true)
+    expect(tx.call!.args[2].value).toEqual(1234567890)
+    expect(tx.call!.args[3].value).toEqual("Abra Shvabra Kadabra")
+    //@ts-ignore
+    expect(tx.call!.args[4].value[0].value).toEqual('base64:UmlkZQ==')
+    //@ts-ignore
+    expect(tx.call!.args[4].value[1].value).toEqual(false)
+    //@ts-ignore
+    expect(tx.call!.args[4].value[2].value).toEqual(223322)
+    //@ts-ignore
+    expect(tx.call!.args[4].value[3].value).toEqual("Porto Franko")
+
+  })
 })
